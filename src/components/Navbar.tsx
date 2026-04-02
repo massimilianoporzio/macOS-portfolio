8641e48700eac3cdf9375e33795485d2700a37d4
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2026-04-03 01:16:15 +0200

    icone a destra --- cercare codice sorgente per switchTheme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,30 @@
+import {navIcons, navLinks} from "#constants";
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">Massimiliano's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({id,name}) => (
+                        <li key={id}><p>
+                            {name}
+                        </p></li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({id,img}) => (
+                        <li key={id}>
+                            <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
     )
 }
 export default Navbar
