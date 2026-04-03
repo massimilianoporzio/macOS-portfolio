01c532f7119bb582a578f7180baf4c774e12ba0d
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2026-04-04 00:17:39 +0200

    fino a renderText portfolio con font variable

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,6 +1,33 @@
+import {type JSX, useRef} from "react";
+const renderText =
+    (text: string, className: string, baseHeight: number = 400): JSX.Element[] => {
+    return [...text].map((char, i) => (
+        <span key={i} className={className} style={{
+            fontVariationSettings: `'wght' ${baseHeight}`,
+        }}>
+        {char === ' ' ? '\u00A0' : char}
+        </span>
+    ))
+}
+
 const Welcome = () => {
+    const titleRef = useRef<HTMLHeadingElement | null>(null);
+    const subtitleRef = useRef<HTMLParagraphElement | null>(null);
     return (
-        <div>Welcome</div>
+        <section id="welcome">
+            <p ref={subtitleRef}>
+                {renderText("Hey, I'm Massimiliano! Welcome to my",
+                    "text-3xl font-georama", 100
+                )} </p>
+            <h1 ref={titleRef} className="mt-7">{
+                renderText("portfolio",
+                    "text-9xl italic font-georama", 400)
+            }</h1>
+
+            <div className="small-screen">
+                <p>This Portfolio is designed for dektop/tablet screens only.</p>
+            </div>
+        </section>
     )
 }
 export default Welcome
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource-variable/georama";
+import "@fontsource-variable/georama/wght-italic.css";
+import "@fontsource-variable/roboto-mono";
 import './index.css'
 import App from './App.tsx'
 
